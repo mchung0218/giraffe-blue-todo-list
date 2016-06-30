5a8b945d1d16ecd98ad5562edee86ec8305b44cc
Anthony Lackey <alackey96@gmail.com>
2016-06-29 22:19:20 -0700

    URL structure setup in views.py & urls.py

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.list, name='list'),
+    url(r'^tasks/$', views.tasks, name='tasks'),
+    url(r'^task/(?P<id>\d+)$', views.task, name='task'),
+    url(r'^task/completed/(?P<id>\d+)$', views.task_completed,
+        name='task_completed'),
+    url(r'^task/importance/(?P<id>\d+)$', views.task_importance,
+        name='task_importance'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,52 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.context_processors import csrf
 
 
+# /
 def list(request):
-    return render(request, 'todolist/list.html', {})
+    c = {}
+    c.update(csrf(request))
+
+    return render(request, 'todolist/list.html', c)
+
+
+# /task/{id}
+def task(request, id):
+
+    # Add task
+    if request.method == 'POST':
+        return JsonResponse({'errorExists': 'false'})
+
+    # Update task
+    elif request.method == 'PUT':
+        return JsonResponse({'errorExists': 'false'})
+
+    # Delete task
+    elif request.method == 'DELETE':
+        return JsonResponse({'errorExists': 'false'})
+
+
+# /task/completed/{id}
+def task_completed(request, id):
+
+    # Mark as completed
+    if request.method == 'PUT':
+        return JsonResponse({'errorExists': 'false'})
+
+
+# /task/importance/{id}
+def task_importance(request, id):
+
+    # Set importance
+    if request.method == 'PUT':
+        return JsonResponse({'errorExists': 'false'})
+
+
+# /tasks
+def tasks(request):
+
+    # Get all tasks
+    if request.method == 'GET':
+        tasks = {'tasks':[{'method': 'Get'}]}
+        return JsonResponse(tasks)
