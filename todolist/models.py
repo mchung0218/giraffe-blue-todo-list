05d17a0fe9f78b2cf9a861d79da6af4cb9906f94
Bradley <blockhar@ualberta.ca>
2016-06-30 18:32:42 -0600

    update textfields to large CharFields

--- todolist/models.py
+++ todolist/models.py
@@ -5,15 +5,16 @@ from django.db import models
 class user(models.Model):
 	username = models.CharField(max_length=30, primary_key=True)
 	email = models.CharField(max_length=30)
-	userpass = models.TextField
+	userpass = models.CharField(max_length=100000)
 
 
 class task(models.Model):
 	username = models.ForeignKey(user)
 	tasknum = models.IntegerField
-	taskdesc = models.TextField
+	taskdesc = models.CharField(max_length=100000)
 	priority = models.CharField(max_length=20)
 	status = models.CharField(max_length=20)
 	
 	class meta:
 		unique_together = (("username", "tasknum"),)
+
