176765845d4889da0c8d5cd9504635fffd2a2350
Bradley <blockhar@ualberta.ca>
2016-06-30 18:07:49 -0600

    added User and task models to todolist

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+	username = models.CharField(max_length=30, primary_key=True)
+	email = models.CharField(max_length=30)
+	userpass = models.TextField
+
+
+class task(models.Model):
+	username = models.ForeignKey(user)
+	tasknum = models.IntegerField
+	taskdesc = models.TextField
+	priority = models.CharField(max_length=20)
+	status = models.CharField(max_length=20)
+	
+	class meta:
+		unique_together = (("username", "tasknum"),)
