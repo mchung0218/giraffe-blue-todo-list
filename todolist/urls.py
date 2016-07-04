2baaa0bb0590354f2eb9eccbd69f1a8b11559806
Bradley <blockhar@ualberta.ca>
2016-07-04 17:47:08 -0600

    rm URLs for update and delete. rm testing URLs

--- todolist/urls.py
+++ todolist/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
         name='task_completed'),
     url(r'^task/priority/(?P<id>\d+)$', views.task_priority,
         name='task_priority'),
-    url(r'^task/update/(?P<id>\d+)$', views.task_update,
-        name='task_update'),
-    url(r'^task/delete/(?P<id>\d+)$', views.task_delete,
-        name='task_delete'),
-    url(r'^task/test/Update$', views.testUpdate, name='testUpdate'), #test URL
-    url(r'^task/test/Delete$', views.testDelete, name='testDelete'), #test URL
+    #url(r'^task/update/(?P<id>\d+)$', views.task_update,
+    #    name='task_update'),
+    #url(r'^task/delete/(?P<id>\d+)$', views.task_delete,
+    #    name='task_delete'),
+    #url(r'^task/test/Update$', views.testUpdate, name='testUpdate'), #test URL
+    #url(r'^task/test/Delete$', views.testDelete, name='testDelete'), #test URL
 ]
