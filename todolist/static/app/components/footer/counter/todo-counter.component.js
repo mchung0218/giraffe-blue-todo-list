61e5b379fe41103085a2fa62ed58b0da28e431f1
Sylphony <symphonictwist@gmail.com>
2016-07-05 04:45:59 -0700

    Added comments for counter and filter

--- todolist/static/app/components/footer/counter/todo-counter.component.js
+++ todolist/static/app/components/footer/counter/todo-counter.component.js
@@ -1,10 +1,20 @@
 "use strict";
 
-function CounterCtrl() {
+/**
+ * CounterCtrl()
+ * The controller for active tasks counter.
+ * @param todo: The todo factory.
+ */
+function CounterCtrl(todo) {
+    var vm = this;
 
+    // 1. Check the todo list
+    // 2. Filter out the tasks that are completed.
+    // 3. Bind a model on the DOM element
 }
 
 
+// Exports
 module.exports = {
     controller: CounterCtrl,
     templateUrl: "/static/app/components/footer/counter/todo-counter.html"
--- todolist/static/app/components/footer/filter/todo-filter.component.js
+++ todolist/static/app/components/footer/filter/todo-filter.component.js
@@ -1,10 +1,16 @@
 "use strict";
 
-function FilterCtrl() {
+/**
+ * FilterCtrl()
+ * The controller for the filter component.
+ */
+function FilterCtrl(todo) {
+    var vm = this;
 
+    // 1. Place an ng-class on the <li> task element which would hide the element depending on what the priority number is.
 }
 
-
+// Exports
 module.exports = {
     controller: FilterCtrl,
     templateUrl: "/static/app/components/footer/filter/todo-filter.html"
