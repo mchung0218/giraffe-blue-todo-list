a2334aa8256995dbb19d657332d957ed3fa1b809
Sylphony <symphonictwist@gmail.com>
2016-07-05 21:12:15 -0700

    Changed cookie and header names to Django's use

--- todolist/static/app/app.config.js
+++ todolist/static/app/app.config.js
@@ -6,7 +6,11 @@
  * @param $httpProvider: The $http provider.
  */
 function httpConfig($httpProvider) {
-    //console.log($httpProvider.defaults);
+    // Change the name of cookie to what Django uses
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+
+     // Change the name of header to what Django uses
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 }
 
 module.exports = httpConfig;
