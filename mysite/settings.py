2d55e0796cb3325e1d279dddf8a9bce1d0f2d52d
Anthony Lackey <alackey96@gmail.com>
2016-07-07 19:24:17 -0700

    Remove: *.sql, sqlite db, sqlite settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,10 +77,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        ### SQLite Settings
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
         ### PostgreSQL Settings
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
          'NAME': 'bluegiraffe',
