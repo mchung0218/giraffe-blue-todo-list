47607bac7e835a12f7b598c44e3fb32e892180be
Sylphony <symphonictwist@gmail.com>
2016-07-07 18:10:31 -0700

    Added enter event for editing task

--- todolist/static/app/build.js
+++ todolist/static/app/build.js
@@ -346,7 +346,7 @@ module.exports = enterEditMode;
 },{}],10:[function(require,module,exports){
 /**
  * exitEditMode()
- * On blur of task edit box, exit edit mode.
+ * On blur/enter of task edit box, exit edit mode.
  */
 function exitEditMode() {
     return {
@@ -355,10 +355,23 @@ function exitEditMode() {
             // Get the task name box
             var theSpan = ele.parent()[0].getElementsByClassName("todo-list__task-name--span")[0];
 
-            ele.on("blur", function() {
+            // Called when blurred or enter key is pressed
+            var blurredFunc = function() {
                 ele.addClass("hide");
 
                 angular.element(theSpan).removeClass("hide");
+            };
+
+            // On blur
+            ele.on("blur", blurredFunc);
+
+            // On keypress
+            ele.on("keypress", function(e) {
+                var code = e.keyCode || e.which;
+
+                if (e.keyCode === 13) {
+                    blurredFunc();
+                }
             });
         }
     };
