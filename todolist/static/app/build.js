dcfd1d2c856a8e20e221759f69bb1c61a155beda
Sylphony <symphonictwist@gmail.com>
2016-07-09 21:55:42 -0700

    Changed user create to JSON content-type

--- todolist/static/app/build.js
+++ todolist/static/app/build.js
@@ -117,21 +117,7 @@ function loginApi($resource) {
     return {
         "User": $resource("/user/"),
 
-        "UserCreate": $resource("/user/create", {}, {
-            save: {
-                "method": "POST",
-                "headers": {
-                    "Content-Type": "application/x-www-form-urlencoded"
-                },
-                "transformRequest": function(obj) {     // Convert the JSON to seralized POST data
-                    var str = [];
-                    for (var p in obj) {
-                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                    }
-                    return str.join("&");
-                }
-            }
-        }),
+        "UserCreate": $resource("/user/create", {}),
 
         "UserLogout": $resource("/user/logout", {}, {
             logout: {
